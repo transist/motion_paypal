84798c94d9c62521808268722d75390fb5cb56d7
Scott Ballantyne <ussballantyne@gmail.com>
2012-10-07 14:13:57 +0800

    is this set up right

--- lib/motion_paypal.rb
+++ lib/motion_paypal.rb
@@ -3,3 +3,18 @@ require "motion_paypal/version"
 module MotionPaypal
   # Your code goes here...
 end
+
+
+['address', 
+ 'advanced_payment', 
+ 'amounts',
+ 'invoice_data',
+ 'invoice_item', 
+ 'payment',
+ 'preapproved_details', 
+ 'receiver_amounts', 
+ 'receiver_payment_details',
+ 'paypal',
+ 'base'].each { |g| 
+   require "motion_paypal/" + g 
+  }
---
+++ lib/motion_paypal/base.rb
@@ -0,0 +1,5 @@
+module MotionPaypal
+  module Base
+    # attr_accessor :name, :street1, :street2, :city, :state, :postalcode, :countrycode
+  end
+end
